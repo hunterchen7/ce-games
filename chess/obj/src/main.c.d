39b5aad0c1abe010e0043de4a7b0c37c0e9b624f
Hunter <hello@hunterchen.ca>
2026-02-20 22:00:51 -0700

    chore: rebuild chess.8xp

--- chess/obj/src/main.c.d
+++ chess/obj/src/main.c.d
@@ -7,10 +7,10 @@ obj/src/main.c.bc: src/main.c /Users/hunterchen/CEdev/include/graphx.h \
   /Users/hunterchen/CEdev/include/sys/util.h \
   /Users/hunterchen/CEdev/include/time.h \
   /Users/hunterchen/CEdev/include/string.h \
-  /Users/hunterchen/CEdev/include/stdio.h \
-  /Users/hunterchen/CEdev/include/stdarg.h \
   /Users/hunterchen/CEdev/include/fileioc.h \
   /Users/hunterchen/CEdev/include/stddef.h \
+  /Users/hunterchen/CEdev/include/stdio.h \
+  /Users/hunterchen/CEdev/include/stdarg.h \
   /Users/hunterchen/CEdev/include/ti/vars.h \
   /Users/hunterchen/CEdev/include/ti/real.h engine/src/engine.h \
   engine/src/book.h engine/src/board.h engine/src/types.h \
